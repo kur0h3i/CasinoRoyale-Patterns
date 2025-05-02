89cf7f261274250d61077cdd6932c4407d908bfb
luisfidelblancograu <luisfidelblancograu@gmail.com>
2025-05-02 17:38:36 +0200

    Added Pasillo.java and temporal Sala.java

---
+++ src/accionesCasino/Pasillo.java
@@ -0,0 +1,31 @@
+package accionesCasino;
+
+import estructuraCasino.Sala;
+import patterns.observer.PullPushModelObservable;
+import patterns.observer.PullPushModelObserver;
+import personas.Jugador;
+
+import java.util.Objects;
+
+public class Pasillo implements PullPushModelObserver {
+
+    private final Integer posX, posY;
+    private final Sala sala; // Necesito a que sala me llevara
+
+    public Pasillo(Integer posX, Integer posY, Sala sala) {
+        this.posX = posX;
+        this.posY = posY;
+        this.sala = sala;
+    }
+
+    @Override
+    public void update(PullPushModelObservable pullPushModelObservable, Object object) {
+        Jugador jugadorTMP = (Jugador) pullPushModelObservable;
+
+        if (Objects.equals(jugadorTMP.getPosX(), this.posX) && Objects.equals(jugadorTMP.getPosY(), this.posY)) {
+            this.sala.setJugador(jugadorTMP);
+            // TODO: Aplicar logica de cambio de sala, la linea anterior ha sido un ejemplo
+        }
+
+    }
+}
---
+++ src/estructuraCasino/Sala.java
@@ -0,0 +1,15 @@
+package estructuraCasino;
+
+import personas.Jugador;
+
+public class Sala {
+
+    private Jugador jugador;
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    // TODO: Lo Requiero esto debido a las multiples salas que vaya a haber
+
+}

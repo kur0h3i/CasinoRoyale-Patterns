63732ffe389a17df83b874b5c1350ac4d762d923
Pedro Perez <pedropm.lolo05@gmail.com>
2025-05-07 06:38:36 +0200

    Rafctor Strategy

--- src/juegos/StrategyJuego.java
+++ src/juegos/StrategyJuego.java
@@ -1,7 +1,21 @@
+
 package juegos;
 
 import excep.ExcepcionJugadorSinFichas;
 
+/**
+ * StrategyJuego => Interfaz que define el contrato para las diferentes
+ * estrategias de juego
+ * disponibles en el casino virtual.
+ * Cada implementación representa un juego concreto (Bingo, Ruleta, Dados, etc.)
+ * y debe proporcionar su propia lógica de partida.
+ */
 public interface StrategyJuego {
+
+    /**
+     * Inicia la partida del juego.
+     *
+     * @throws ExcepcionJugadorSinFichas si el jugador no tiene fichas disponibles
+     */
     void iniciarPartida() throws ExcepcionJugadorSinFichas;
 }

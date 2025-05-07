9febeb92f33cb680fb6dfef0f7cb1e87d75bf8c3
Pedro Perez <pedropm.lolo05@gmail.com>
2025-05-07 06:36:27 +0200

    Refactor Items

--- src/items/Items.java
+++ src/items/Items.java
@@ -1,12 +1,37 @@
+
 package items;
 
+/**
+ * Interfaz Items => Define el contrato para todos los objetos jugables o
+ * consumibles.
+ * Contiene métodos para obtener propiedades básicas y ejecutar la acción del
+ * ítem.
+ */
 public interface Items {
-    
+
+    /**
+     * Obtiene el nombre del ítem.
+     * 
+     * @return nombre como String
+     */
     String getNombre();
 
+    /**
+     * Obtiene el precio en euros del ítem.
+     * 
+     * @return precio como Double
+     */
     Double getPrecio();
 
+    /**
+     * Obtiene la descripción o efecto del ítem.
+     * 
+     * @return descripción como String
+     */
     String getDescripcion();
 
+    /**
+     * Acción principal al usar el ítem: define el comportamiento específico.
+     */
     void usar();
 }
